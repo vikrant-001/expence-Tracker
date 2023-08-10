7f1083f37eae782197dbd5ca1c77e89adba7a1ee
vikrant-001 <121452566+vikrant-001@users.noreply.github.com>
2023-08-10 16:29:16 +0530

    filter Added

---
+++ src/component/Expence/ExpenceFilter.jsx
@@ -0,0 +1,21 @@
+import "./ExpenceFilter.css"
+const ExpenceFilter = (props) => {
+    const dropDownHandler = (event) => {
+        props.onFilterChange(event.target.value);
+    }
+    return (
+        <div className="expenses-filter">
+            <div className="expenses-filter__control">
+                <label>Filter By Year</label>
+                <select value={props.selected} onChange={dropDownHandler}>
+                    <option value={2022}>2022</option>
+                    <option value={2021}>2022</option>
+                    <option value={2020}>2022</option>
+                    <option value={2019}>2022</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenceFilter;
--- src/component/Expence/Expences.jsx
+++ src/component/Expence/Expences.jsx
@@ -1,10 +1,19 @@
 import ExpenceItem from './ExpenceItem';
 import Card from '../UI/Card'
 import './Expence.css';
+import ExpenceFilter from './ExpenceFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+  const [filteredYear,setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = (selectfiltered) => {
+    setFilteredYear(selectfiltered);
+  };
+
   return (
     <Card className="expenses">
+    <ExpenceFilter selected = {filteredYear} onFilterChange = {filterChangeHandler}/>
       <ExpenceItem
         title={props.items[0].title}
         amount={props.items[0].amount}
